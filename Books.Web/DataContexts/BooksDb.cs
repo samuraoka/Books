8b4830181c670fdd53ee804c21269af1c191b837
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-02-09 07:14:24 +0900

    Generated code first migration files.

--- Books.Web/DataContexts/BooksDb.cs
+++ Books.Web/DataContexts/BooksDb.cs
@@ -3,6 +3,13 @@ using System.Data.Entity;
 
 namespace Books.Web.DataContexts
 {
+    // PM> get-help entityframework
+    // PM> get-help enable-migrations
+    // PM> Enable-Migrations -ContextTypeName Books.Web.DataContexts.BooksDb -MigrationsDirectory DataContexts\BookMigrations
+    // PM> get-help add-migration
+    // PM> Add-Migration -Name InitialCreate -ConfigurationTypeName Books.Web.DataContexts.BookMigrations.Configuration
+    // PM> get-help update-database
+    // PM> Update-Database -ConfigurationTypeName Books.Web.DataContexts.BookMigrations.Configuration
     public class BooksDb : DbContext
     {
         public BooksDb()
--- Books.Web/DataContexts/IdentityDb.cs
+++ Books.Web/DataContexts/IdentityDb.cs
@@ -3,6 +3,13 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Books.Web.DataContexts
 {
+    // PM> get-help entityframework
+    // PM> get-help enable-migrations
+    // PM> Enable-Migrations -ContextTypeName Books.Web.DataContexts.IdentityDb -MigrationsDirectory DataContexts\IdentityMigrations
+    // PM> get-help add-migration
+    // PM> Add-Migration -Name InitialCreate -ConfigurationTypeName Books.Web.DataContexts.IdentityMigrations.Configuration
+    // PM> get-help update-database
+    // PM> Update-Database -ConfigurationTypeName Books.Web.DataContexts.IdentityMigrations.Configuration
     public class IdentityDb : IdentityDbContext<ApplicationUser>
     {
         public IdentityDb()
