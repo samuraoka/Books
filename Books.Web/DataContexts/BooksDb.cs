47633bd3be60b27ec6bea7ab37e477439054b22e
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-02-10 05:34:40 +0900

    Added logging capability

--- Books.Web/DataContexts/BooksDb.cs
+++ Books.Web/DataContexts/BooksDb.cs
@@ -1,5 +1,6 @@
 ﻿using Books.Entities;
 using System.Data.Entity;
+using System.Diagnostics;
 
 namespace Books.Web.DataContexts
 {
@@ -15,6 +16,7 @@ namespace Books.Web.DataContexts
         public BooksDb()
             : base("DefaultConnection")
         {
+            Database.Log = sql => Debug.Write(sql);
         }
 
         public DbSet<Book> Books { get; set; }
