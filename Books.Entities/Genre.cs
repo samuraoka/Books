cf74f5cf2be1ce41374489d391d06d5b744e30d1
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-02-09 13:23:02 +0900

    Added Genre enum display name attribute. Added a link to Books page.

--- Books.Entities/Genre.cs
+++ Books.Entities/Genre.cs
@@ -1,10 +1,16 @@
-﻿namespace Books.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Entities
 {
     public enum Genre
     {
+        // MVC EnumDropdownListFor bind with enum example 
+        // http://www.advancesharp.com/blog/1163/mvc-enumdropdownlistfor-bind-with-enum-example
+        [Display(Name = "Non Fiction")]
         NonFiction,
         Romance,
         Action,
+        [Display(Name = "Science Fiction")]
         ScienceFiction,
     }
 }
---
+++ Books.Web/Helpers/GenreHelpers.cs
@@ -0,0 +1,27 @@
+﻿using Books.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Books.Web.Helpers
+{
+    public static class GenreHelpers
+    {
+        public static string GetDisplayName(this Genre value)
+        {
+            var result = value.ToString();
+
+            var attribute = value.GetType().GetField(result)
+                .GetCustomAttributes(inherit: false)
+                .OfType<DisplayAttribute>()
+                .FirstOrDefault();
+
+            if (attribute != null)
+            {
+                result = attribute.GetName();
+            }
+
+            return result;
+        }
+
+    }
+}
